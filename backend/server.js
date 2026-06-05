92707692d9398fcab98b398d63a5675cb6930658
bhavya-borad <bhavyaborad@gmail.com>
2026-06-06 03:04:04 +0530

    fixed path

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.use(express.json());
